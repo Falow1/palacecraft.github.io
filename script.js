c13a45691053f6432ebe81818a4247b0d8148a97
Falow1 <92454215+Falow1@users.noreply.github.com>
2024-06-13 22:05:21 -0300

    Menu lateral de apenas um click no celular

--- script.js
+++ script.js
@@ -2,60 +2,38 @@ document.addEventListener('DOMContentLoaded', function() {
     var navItems = document.querySelectorAll('.sidebar-nav > ul > li');
   
     navItems.forEach(function(item) {
-      item.addEventListener('click', function(e) {
+      var touchTimeout;
+      
+      function handleClick(e) {
         var isOpen = this.classList.contains('open');
-
         navItems.forEach(function(i) {
           i.classList.remove('open');
         });
-
         if (!isOpen) {
           this.classList.add('open');
         }
+      }
+  
+      item.addEventListener('click', handleClick);
+      
+      item.addEventListener('touchstart', function(e) {
+        touchTimeout = setTimeout(function() {
+          handleClick.call(item, e);
+        }, 500); // Tempo para toque longo
       });
   
       item.addEventListener('touchend', function(e) {
-        e.preventDefault();
-        var isOpen = this.classList.contains('open');
+        if (touchTimeout) {
+          clearTimeout(touchTimeout);
+          handleClick.call(item, e);
+        }
+      });
   
-        navItems.forEach(function(i) {
-          i.classList.remove('open');
-        });
-  
-        if (!isOpen) {
-          this.classList.add('open');
+      item.addEventListener('touchmove', function(e) {
+        if (touchTimeout) {
+          clearTimeout(touchTimeout);
         }
       });
     });
   });
-document.addEventListener('DOMContentLoaded', function() {
-  var navItems = document.querySelectorAll('.sidebar-nav > ul > li');
-
-  navItems.forEach(function(item) {
-    item.addEventListener('click', function(e) {
-      var isOpen = this.classList.contains('open');
-
-      navItems.forEach(function(i) {
-        i.classList.remove('open');
-      });
-
-      if (!isOpen) {
-        this.classList.add('open');
-      }
-    });
-
-    item.addEventListener('touchend', function(e) {
-      e.preventDefault();
-      var isOpen = this.classList.contains('open');
-
-      navItems.forEach(function(i) {
-        i.classList.remove('open');
-      });
-
-      if (!isOpen) {
-        this.classList.add('open');
-      }
-    });
-  });
-});
   
