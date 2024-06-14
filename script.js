3ee70c71f44eee95b785f5a09b0c018392401d12
Falow1 <92454215+Falow1@users.noreply.github.com>
2024-06-13 22:08:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,59 @@ document.addEventListener('DOMContentLoaded', function() {
     var navItems = document.querySelectorAll('.sidebar-nav > ul > li');
   
     navItems.forEach(function(item) {
-      var touchTimeout;
-      
-      function handleClick(e) {
+      item.addEventListener('click', function(e) {
         var isOpen = this.classList.contains('open');
+
         navItems.forEach(function(i) {
           i.classList.remove('open');
         });
+
         if (!isOpen) {
           this.classList.add('open');
         }
-      }
-  
-      item.addEventListener('click', handleClick);
-      
-      item.addEventListener('touchstart', function(e) {
-        touchTimeout = setTimeout(function() {
-          handleClick.call(item, e);
-        }, 500); // Tempo para toque longo
       });
   
       item.addEventListener('touchend', function(e) {
-        if (touchTimeout) {
-          clearTimeout(touchTimeout);
-          handleClick.call(item, e);
-        }
-      });
+        e.preventDefault();
+        var isOpen = this.classList.contains('open');
   
-      item.addEventListener('touchmove', function(e) {
-        if (touchTimeout) {
-          clearTimeout(touchTimeout);
+        navItems.forEach(function(i) {
+          i.classList.remove('open');
+        });
+  
+        if (!isOpen) {
+          this.classList.add('open');
         }
       });
     });
   });
-  
+document.addEventListener('DOMContentLoaded', function() {
+  var navItems = document.querySelectorAll('.sidebar-nav > ul > li');
+
+  navItems.forEach(function(item) {
+    item.addEventListener('click', function(e) {
+      var isOpen = this.classList.contains('open');
+
+      navItems.forEach(function(i) {
+        i.classList.remove('open');
+      });
+
+      if (!isOpen) {
+        this.classList.add('open');
+      }
+    });
+
+    item.addEventListener('touchend', function(e) {
+      e.preventDefault();
+      var isOpen = this.classList.contains('open');
+
+      navItems.forEach(function(i) {
+        i.classList.remove('open');
+      });
+
+      if (!isOpen) {
+        this.classList.add('open');
+      }
+    });
+  });
+});
